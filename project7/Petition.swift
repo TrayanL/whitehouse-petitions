6d5129c89aae9d2e4be6d37443ed8c4e2a96c320
Trayan Lazarov <trayan.officiel@icloud.com>
2022-06-30 16:40:46 +0300

    Adding project

---
+++ project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Trayan Lazarov on 17.05.22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Trayan Lazarov on 17.05.22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
